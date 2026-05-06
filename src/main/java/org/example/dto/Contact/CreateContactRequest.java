51bb644957873769f241bf0b89ac5b38d80fb50c
Gleb2503 <fedorovgl2503@gmail.com>
2026-05-06 13:39:26 +0300

    +

--- src/main/java/org/example/dto/Contact/CreateContactRequest.java
+++ src/main/java/org/example/dto/Contact/CreateContactRequest.java
@@ -1,7 +1,7 @@
 package org.example.dto.Contact;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotNull;  // ✅ Импорт должен быть!
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 import org.example.entity.Contact;
